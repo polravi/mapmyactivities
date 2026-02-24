bb97d43cf300ee472a73349dc5c9d1f8bdf26904
polravi <polravi@gmail.com>
2026-02-24 13:56:50 -0600

    Modify Firebase config with new credentials

--- apps/web/firebase-config.js
+++ apps/web/firebase-config.js
@@ -1,9 +1,9 @@
   {
-    "apiKey":"AIzaSyA5zfbGx60xUdrdEDI57v4CBj5Ika-W3fU",
+    "apiKey":"-as",
     "authDomain": "mapmyactivities.firebaseapp.com",
     "projectId": "mapmyactivities",
     "storageBucket": "mapmyactivities.firebasestorage.app",
-    "messagingSenderId": "1034754138696",
-    "appId": "1:1034754138696:web:ce491cd626ed1d5500ee7d",
-    "measurementId": "G-J8RRXH7WGC"
+    "messagingSenderId": "333",
+    "appId": "1:ee:web:se",
+    "measurementId": "G-34"
   }
